2398a1009d9197ae6d723c0f104e9dba410f4e86
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-28 17:55:05 +0530

    Added: peak and pop operation stack till empty

--- src/com/StackMain.java
+++ src/com/StackMain.java
@@ -20,19 +20,30 @@ public class StackMain {
 	                    flag = true;
 	                    break;
 	                case 2:
-	                    stackOperation.display();
+	                    stackOperation.pop();
 	                    flag = true;
 	                    break;
 	                case 3:
+	                    System.out.printf("Top element is %d", +stackOperation.peek());
+	                    System.out.println();
+	                    flag = true;
+	                    break;
+	                case 4:
+	                    stackOperation.display();
+	                    flag = true;
+	                    break;
+	                case 5:
 	                    flag = false;
 	                    break;
 	            }
 	        }
 	    }
 	    public static int userMenu () {
-	        System.out.println("Press 1 to Insert");
-	        System.out.println("Press 2 to Display");
-	        System.out.println("Press 3 to Quit");
+	        System.out.println("Press 1 to Push");
+	        System.out.println("Press 2 to Pop");
+	        System.out.println("Press 3 to Peek");
+	        System.out.println("Press 4 to Display");
+	        System.out.println("Press 5 to Quit");
 
 	        int choice = input.nextInt();
 
--- src/com/StackOperation.java
+++ src/com/StackOperation.java
@@ -3,33 +3,51 @@ package com;
 public class StackOperation {
 	private Node top;
 
-	public StackOperation() {
-		this.top = top;
-	}
+    public StackOperation() {
+        this.top = top;
+    }
 
-	public void push(int element) {
-		Node node = new Node();
-		node.data = element;
-		if(node == null) {
-			//System.out.println("\n" + "Stack is Empty");
-			node.next = null;
-		}
-		else {
-			node.next = top;
-			top = node;
-		}
-	}
+    public void push(int element) {
+        Node node = new Node();
+        node.data = element;
+        if(node == null)
+            node.next = null;
+        else {
+            node.next = top;
+            top = node;
+        }
+    }
+    void pop()
+    {
+        if(top == null)
+            System.out.println("\n" + "Stack is Empty!");
+        else{
+            Node temp = top;
+            System.out.printf("Deleted element: %d", temp.data);
+            System.out.println();
+            top = temp.next;
+        }
+    }
 
-	public void display() {
-		if(top == null)
-			System.out.println("Stack is Empty");
-		else {
-			Node temp = top;
-			while(temp.next != null) {
-				System.out.println(temp.data);
-				temp = temp.next;
-			}
-			System.out.println(temp.data);
-		}
-	}
+    public int peek() {
+        if (top != null)
+            return top.data;
+        else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
+
+    public void display() {
+        if(top == null)
+            System.out.println("Stack is Empty");
+        else {
+            Node temp = top;
+            while(temp.next != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+            System.out.println(temp.data);
+        }
+    }
 }
